5af776fb3c1289b76d6b059471eda6db2e00ba89
Tanner Keyes <Tanner.keyes@stu.jefferson.kyschools.us>
2016-03-29 10:59:21 -0400

    Initialize Player Class

---
+++ Player.java
@@ -0,0 +1,24 @@
+
+public class Player {
+	int score;
+
+	public static void addScore() {
+
+	}
+
+	public static void resetScore() {
+
+	}
+
+	public static int printScore() {
+
+	}
+
+	public static void getStanding() {
+
+	}
+
+	public static void setStanding() {
+
+	}
+}
