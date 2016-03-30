4126b20726045a29b16e44d232120d643c7a99ad
Tanner Keyes <Tanner.keyes@stu.jefferson.kyschools.us>
2016-03-30 11:11:12 -0400

    Added dice animation

--- Dice.java
+++ Dice.java
@@ -12,7 +12,67 @@ public class Dice{
 		rolled[1] = rand.nextInt(6);
 		return rolled;
 	}
-	
+
+	public String toString(int die){
+		String output = "";
+		if(die == 1){
+			output += "------------\n";
+			output += "|           |\n";
+			output += "|           |\n";
+			output += "|     *     |\n";
+			output += "|           |\n";
+			output += "|           |\n";
+			output += "------------\n";
+		}
+		if(die == 2){
+			output += "------------\n";
+			output += "|           |\n";
+			output += "|   *       |\n";
+			output += "|           |\n";
+			output += "|        *  |\n";
+			output += "|           |\n";
+			output += "------------\n";
+		}
+		if(die == 3){
+			output += "------------\n";
+			output += "|           |\n";
+			output += "|  *        |\n";
+			output += "|     *     |\n";
+			output += "|        *  |\n";
+			output += "|           |\n";
+			output += "------------\n";
+		}
+		if(die == 4){
+			output += "------------\n";
+			output += "|           |\n";
+			output += "|   *    *  |\n";
+			output += "|           |\n";
+			output += "|   *    *  |\n";
+			output += "|           |\n";
+			output += "------------\n";
+		}
+		if(die == 5){
+			output += "------------\n";
+			output += "|           |\n";
+			output += "|  *     *  |\n";
+			output += "|     *     |\n";
+			output += "|  *     *  |\n";
+			output += "|           |\n";
+			output += "------------\n";
+		}
+		if(die == 6){
+			output += "------------\n";
+			output += "|           |\n";
+			output += "|  *     *  |\n";
+			output += "|  *     *  |\n";
+			output += "|  *     *  |\n";
+			output += "|           |\n";
+			output += "------------\n";
+		}
+		
+		return output;
+	}
+
 	public static int checkEnd(int num1, int num2){
 		if(((num1 + num2) % 6 == 2) && ((num1 + num2) > 6)) {
 			return 1;
