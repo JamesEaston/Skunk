7bea0e64827145b22914eea885f80a2a48fc31de
Tanner Keyes <Tanner.keyes@stu.jefferson.kyschools.us>
2016-03-30 10:47:54 -0400

    Added checkEnd method

--- Dice.java
+++ Dice.java
@@ -12,4 +12,20 @@ public class Dice{
 		rolled[1] = rand.nextInt(6);
 		return rolled;
 	}
+	
+	public static int checkEnd(int num1, int num2){
+		if(((num1 + num2) % 6 == 2) && ((num1 + num2) > 6)) {
+			return 1;
+		}
+
+		if(((num1 + num2) % 5 == 3) && ((num1 + num2) > 5)) {
+			return 1;
+		}
+
+		if((num1 + num2 == 2)) {
+			return 2;
+		}
+		
+		return 0;
+	}
 }
